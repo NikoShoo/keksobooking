8d0d88ae24155ae9c7a01d9778f9989de3951b90
NikoShoo <NikoWork1@ya.ru>
2025-09-22 16:44:19 +0400

    Добавляет карту

---
+++ js/map.js
@@ -0,0 +1,25 @@
+export const setMap = () => new Promise((resolve, reject) => {
+	const map = L.map('map-canvas')
+		.on('load', () => {
+			console.log('Карта инициализирована');
+			resolve();
+		})
+		.setView({
+			lat: 59.92749,
+			lng: 30.31127,
+		}, 10);
+
+	L.tileLayer(
+		'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		},
+	).addTo(map);
+
+	const marker = L.marker(
+		{
+			lat: 59.96831,
+			lng: 30.31748,
+		},
+	);
+})
